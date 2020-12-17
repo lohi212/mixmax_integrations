fad32ad2f9592db7ac2444c7c6115c242c19645b
lohi <suvvarilohita@gmail.com>
2020-12-17 14:47:09 +0530

    mixmax email fetch

---
+++ server.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+
+
+fetch('https://api.mixmax.com/v1/contacts', {
+	/*method: 'POST',
+	headers: {
+		"Content-Type": "application/json",
+        "X-API-Token": "ab8b4db7-02da-405c-a593-72a52e6998a7",
+    }
+	results: 
+   JSON.stringify( {
+       { property: 'email', value: 'testingapis@hubspot.com' },
+        { property: 'name', value: 'test' },
+        { property: 'lastname', value: 'testerson' },
+        },
+  json: true };*/
+    method: 'GET',
+    headers: {
+        "X-API-Token": "ab8b4db7-02da-405c-a593-72a52e6998a7",
+    }
+}).then(res => res.json())
+.then(json => {
+    console.log("Email :",json.results[0].email);
+	console.log(json);
+})
+.catch(err => {
+  console.error(err);
+});
